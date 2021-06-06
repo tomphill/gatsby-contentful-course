a15c7b9f8d2340172c10b7f6b61f456db4c439c3
tom <tomphill@live.co.uk>
2021-06-06 14:06:29 +0800

    add gatsby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
         },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
+        `gatsby-plugin-image`,
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
